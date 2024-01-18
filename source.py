68073fe5248aebf354d6e00a4e869aebc4188788
Tatjana Bišarova <144550001+TatjanaBisarova@users.noreply.github.com>
2024-01-18 22:58:58 +0200

    Update source.py

--- source.py
+++ source.py
@@ -1,1 +1,36 @@
 
+from openpyxl import load_workbook
+
+wb = load_workbook('S_010123_310523.xlsx')
+ws = wb['01-05.2023']
+max_row = ws.max_row
+
+summa = 0
+
+for row in ws.iter_rows(min_row=2, max_col=3, max_row=max_row):
+    for cell in row:
+        if str(cell.value).startswith('B'):
+ 
+            summa += float(cell.offset(column=3).value)
+
+
+print(f"S_010123_310523 banka,EUR: {round(summa, 2)}")
+
+wb.close()
+
+wb_L = load_workbook('L_01_05.23.xlsx')
+ws_L = wb_L['Banka']
+max_row_L = ws_L.max_row
+
+summa_L = 0
+
+
+for row in ws_L.iter_rows(min_row=3, max_col=3, max_row=max_row_L):
+  for cell in row:
+    if cell.value is not None:
+      if isinstance(cell.value, (int, float)):
+        summa_L += cell.value
+
+print(f"L_01_05.23 banka, EUR: {round(summa_L, 2)}")
+
+wb_L.close()
